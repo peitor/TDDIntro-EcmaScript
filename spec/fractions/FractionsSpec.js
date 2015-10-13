a623aeca83397689ee07272bfd4a40847dd4e9e6
Peter Gfader <pgf@zuehlke.com>
2015-10-13 10:01:25 +0200

    added todolist, removed dummy test

--- spec/fractions/FractionsSpec.js
+++ spec/fractions/FractionsSpec.js
@@ -1,13 +1,5 @@
 'use strict';
 
-describe("A test suite", function () {
-  it("contains spec with an expectation", function () {
-    // failing test?
-    expect(true).toBe(true);
-  });
-});
-
-
 describe("New Fraction creation", function () {
   var Fraction = require('../../lib/fractions/Fraction');
   var f;
@@ -23,6 +15,22 @@ describe("New Fraction creation", function () {
     expect(f.denominator()).toEqual(2);
   });
 });
+
+
+
+// TODO LIST
+        // 0/1 + 0/2 = 0/??   0 nominator addition
+        // 1/1 + 1/1 = 2/1    1 denominator addition
+        // 1/4 + 2/4 = 3/4    same denominator
+        // 1/3 + 2/3 = 3/3    result full number
+        // 1/2 + 1/3 = 5/6    
+        // 7/3 + 4/5 = 47/15  complex example, no simplification
+        // 3/3 + 4/4 = ??     whole numbers, expressed as fraction
+        // 3 + 4 = 7          whole integers  
+        // 5/2 +           
+        // 1/3 + 1/2 = 5/6    denominator expansion
+        // 1/0 ??             Error - 0 division
+        // 2 1/2 ??           Error - not allowed
 
 
 
