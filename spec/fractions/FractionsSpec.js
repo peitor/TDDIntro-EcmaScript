13d1e24afe66e7fbcc3f5e8715d4ab972ea4bd30
Peter Gfader <pgf@zuehlke.com>
2015-10-13 09:58:37 +0200

    defined fraction api via tests

--- spec/fractions/FractionsSpec.js
+++ spec/fractions/FractionsSpec.js
@@ -1,9 +1,26 @@
 'use strict';
 
-describe("A test suite", function() {
-  it("contains spec with an expectation", function() {
-	  // failing test?
+describe("A test suite", function () {
+  it("contains spec with an expectation", function () {
+    // failing test?
     expect(true).toBe(true);
+  });
+});
+
+
+describe("New Fraction creation", function () {
+  var Fraction = require('../../lib/fractions/Fraction');
+  var sum;
+  beforeEach(function () {
+    sum = new Fraction(1, 2);
+  });
+
+  it("should return 1 as nominator", function () {
+    expect(sum.nominator()).toEqual(1);
+  });
+
+  it("should return 2 as denominator", function () {
+    expect(sum.denominator()).toEqual(2);
   });
 });
 
